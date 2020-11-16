25e4ebb3ac1d29fa757e1c6eff0baa76d2d711d1
TyyHunt <tyhunt04@gmail.com>
2020-11-15 17:55:49 -0700

    added index.js and jwt-config.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const userRoutes = require('./routes/user.routes');
+const middleware = require('./middleware/errors.middleware');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const logLevel = process.env.LOG_LEVEL || 'dev';
+
+app.use(logger(logLevel));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/tasks', tasksRoutes);
+
+app.use(middleware.error404);
+
+app.use(middleware.error500);
+
+app.listen(port, function() {
+    console.log(`Running on port: ${port}...`)
+})
+
